f6b3759a99fe25cf1fa4dd8a54c0b3b95894a3be
Trey Schmohl <jms4mx@virginia.edu>
2015-11-08 23:46:31 -0500

    fixed one issue in hs.py

--- code/python/hs.py
+++ code/python/hs.py
@@ -1,20 +1,21 @@
-if (n > 0 and n == round(n)):
-    length = 1
-    print (n)
-    while (n != 1):
-        if ((n % 2) == 0):
-            n = n//2
-            print(n)
-        else:
-            n = 3*n+1
-            print(n)
-        length = length + 1
-    times = 1
-    while (n == 1 and times == 1):
-        print(length)
-        print(None)
-        times = (times -1)
-else: print("ERROR")
+def hs(n):
+    if (n > 0 and n == round(n)):
+        length = 1
+        print (n)
+        while (n != 1):
+            if ((n % 2) == 0):
+                n = n//2
+                print(n)
+            else:
+                n = 3*n+1
+                print(n)
+            length = length + 1
+        times = 1
+        while (n == 1 and times == 1):
+            print(length)
+            print(None)
+            times = (times -1)
+    else: print("ERROR")
 
         
     
